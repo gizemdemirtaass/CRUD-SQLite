b6cfc0623cff75310fee2128afa5847da527a9ca
gizemdemirtaass <116717342+gizemdemirtas@users.noreply.github.com>
2024-09-16 16:10:50 +0300

    Initial Commit

---
+++ CRUD-SQLite/CRUD_SQLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUD_SQLiteApp.swift
+//  CRUD-SQLite
+//
+//  Created by gizem demirtas on 16.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CRUD_SQLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
